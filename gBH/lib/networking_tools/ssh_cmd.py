b9fe0ba207f11b6a5e6122c039ed082bea5e2134
Garrett <111312204+gpears98@users.noreply.github.com>
2023-02-02 19:09:00 -0600

    Create ssh_cmd.py

---
+++ gBH/lib/networking_tools/ssh_cmd.py
@@ -0,0 +1,13 @@
+import paramiko
+
+def ssh_command(ip, port, user, passwd, cmd):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, port=port, username=user, password= passwd)
+    
+    _, stdout, stderr = client.exec_command(cmd)
+    output = stdout.readlines() + stderr.readlines()
+    if output:
+        print('-output-')
+        for line in output:
+            print(line.strip())
